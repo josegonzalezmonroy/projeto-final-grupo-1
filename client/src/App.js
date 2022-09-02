91026902f3e0d3fa00e18aec6925db429232d526
manuelcatia20@gmail.com <manuelcatia20@gmail.com>
2022-09-02 15:17:07 -0300

    Istalei o pacote do Material UI para nos dar todos os benefícions de formato do template. Aí fica mais fácil a Gaby encaixar as imagens.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
+import Inicio from './Pages/Cliente/Templeites';
 
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="App">
+      {/*<h1>OLÁ MUNDO!!!</h1>*/}
+      <Inicio/>
+    </div>
   );
 }
 
