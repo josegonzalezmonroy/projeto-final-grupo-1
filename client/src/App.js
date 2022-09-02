a668337292b70df269144372bc4c60a4fc2c1e11
manuelcatia20@gmail.com <manuelcatia20@gmail.com>
2022-09-02 14:16:52 -0300

    Clonei o projeto de novo rsrsrr

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./Components/Navbar";
+
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route></Route>
-        </Routes>
-      </BrowserRouter>
+      
     </>
   );
 }
