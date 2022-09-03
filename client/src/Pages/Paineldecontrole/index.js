ac5e484c10fd7b4ee07ad508d219dc2a673993b3
manuelcatia20@gmail.com <manuelcatia20@gmail.com>
2022-09-03 19:20:37 -0300

    Vou atualizando tudo que fizemos até aqui.

---
+++ client/src/Pages/Paineldecontrole/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Paineldecontrole() {
+  return (
+    <div>
+      <h1>Paineldecontrole!</h1>
+      
+    </div>
+  );
+}
+
+export default Paineldecontrole;
---
+++ client/src/Pages/Produtos/Cadastro.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function CadastraProdutos(){
+    return(
+        <div>Cadastrar Produtos</div>
+    );
+}
