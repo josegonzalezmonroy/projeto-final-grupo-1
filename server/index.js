c0cffd06aff4bda5aaa8319184192e38a58042f9
Jose <105132443+josegonzalezmonroy@users.noreply.github.com>
2022-09-04 15:50:43 -0300

    iniciando os testes com routes

--- server/index.js
+++ server/index.js
@@ -4,34 +4,19 @@ const app = express()
 const dataBase = 'market'
 const port = 3001 //foi usada a porta 3000 para se diferenciar da porta 3000 do React
 const clientPort = 3000 //dominio do cliente
-const Product = require('./models/Product')//importando nosso arquivo Produto
+const routerCrud = require('./routes/crud')
 
+//cors para permitir o acesso do dominio do cliente no nosso servidor
 const cors = require('cors')
 app.use(cors({
     origin: `http://localhost:${clientPort}`
 }))
 
-app.get('/create', async (req, res) => {
-    const product = await Product.create({
-        name: 'Calça',
-        description: 'A melhor calça do mundo',
-        price: 120,
-        category: 'Roupas'
-    })
-    res.json({ product })
-})
+app.use(express.json())
 
-app.get('/read', async(req, res)=>{
-    const products = await Product.find({})
-    res.json({ products })
-})
+app.use(routerCrud)//rotas
 
-
-
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+app.use('/', routerCrud) //rota principal
 
 mongoose.connect(`mongodb+srv://jose:1234@cluster0.5z55mop.mongodb.net/${dataBase}?retryWrites=true&w=majority`)
     .then(() => {
---
+++ server/routes/crud.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+const Product = require('../models/Product')//importando nosso arquivo Produto
+
+
+//create
+router.post('/create', async (req, res) => {
+
+    const name = req.body.name
+    const description = req.body.description
+    const price = req.body.price
+    const category = req.body.category
+
+    const product = new Product({
+        name,
+        description,
+        price,
+        category
+    }) 
+    await product.save().then(console.log(req.body, 'Produto'))
+
+    return res.status(201).json(req.body)
+})
+
+//Read
+router.get('/read', async (req, res) => {
+    const products = await Product.find({})
+    res.json({ products })
+})
+
+//Update
+ 
+//Delete
+
+module.exports = router
