01490f710b21aef3655c673b85f84ade2d86ea34
Jose <105132443+josegonzalezmonroy@users.noreply.github.com>
2022-09-05 02:43:15 -0300

    reorganizando o index

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const dataBase = 'market'
+const dataBase = 'projeto-teste'
 const port = 3001 //foi usada a porta 3000 para se diferenciar da porta 3000 do React
 const clientPort = 3000 //dominio do cliente
-const routerCrud = require('./routes/crud')
+const routerCrud = require('./routes/crud') 
 
 //cors para permitir o acesso do dominio do cliente no nosso servidor
 const cors = require('cors')
@@ -15,7 +15,6 @@ app.use(cors({
 app.use(express.json())
 
 app.use(routerCrud)//rotas
-
 app.use('/', routerCrud) //rota principal
 
 mongoose.connect(`mongodb+srv://jose:1234@cluster0.5z55mop.mongodb.net/${dataBase}?retryWrites=true&w=majority`)
