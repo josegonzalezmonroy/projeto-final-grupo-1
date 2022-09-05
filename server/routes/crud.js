42cce3e86faaea75dd74792b11cc1726b9466ad8
Jose <105132443+josegonzalezmonroy@users.noreply.github.com>
2022-09-05 02:40:31 -0300

    acrescentando funcao para mostrar as collections

--- server/routes/crud.js
+++ server/routes/crud.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')//mongoose para listar as collections
 const router = express.Router()
 const Product = require('../models/Product')//importando nosso arquivo Produto
 
@@ -16,20 +17,39 @@ router.post('/create', async (req, res) => {
         description,
         price,
         category
-    }) 
+    })
     await product.save().then(console.log(req.body, 'Produto'))
 
     return res.status(201).json(req.body)
 })
 
+//read collections
+router.get('/show', async (req, res) => {
+    mongoose.connection.db.listCollections().toArray(function (err, names) {
+        console.log(names)
+        res.send(names)
+    })
+    
+    return res.status(200)
+}) 
+
+
+
 //Read
 router.get('/', async (req, res) => {
     const products = await Product.find({})
-    res.json(products)
+    return res.json(products)
+})
+
+//details
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const produto = await Product.findById(id)
+    return res.json([produto])//[] para o map poder funcionar no front
 })
 
 //Update
- 
+
 //Delete
 
 module.exports = router
