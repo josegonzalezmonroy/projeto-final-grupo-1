986da355c6527fcf3906a94a06024cbaa03320d2
Gabriela Zapata <gabyzda@gmail.com>
2022-09-07 15:57:20 -0300

    Modificado idioma das variaveis

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({//criamos o Schema para definir como serão os documentos em nossa base de dados
-    name: String,
-    description: String,
-    price: Number,
-    category: String
+    nome: String,
+    descricao: String,
+    preco: Number,
+    categoria: String
 })
 
 const Product = mongoose.model('Product', productSchema)//Product sera nossa classe para criar os novos produtos
