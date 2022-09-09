3e2fc9412724af86cd0fba56803548433cdff0e1
woodthestrong <woodthestrong@gmail.com>
2022-09-08 21:31:20 -0300

    C:/Program Files/Git/projeto-final-grupo-1-4

--- client/src/Components/Header/Index.js
+++ client/src/Components/Header/Index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Index.css';
-
+import ButtonIcon from "../Button/buttons";
 const Header=({title}) =>(
     
 <header>
     <h1 className='font-weigt-bold'>{title?title:""}</h1>
+    <div className = "button-add">
+        <ButtonIcon icon="shopping-cart" />
+    </div>
 </header>
 );
 
--- client/src/Components/PainelDeControle/CadastrarProduto/CadastrarProduto.js
+++ client/src/Components/PainelDeControle/CadastrarProduto/CadastrarProduto.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+//import { AddShoppingCartIcon } from "@mui/icons-material/AddShoppingCart";
+
 
 export default function CadastrarProduto() {
 
@@ -26,40 +28,39 @@ export default function CadastrarProduto() {
 
     return (
         <div>
-            <h2 className="font-weight-bold text-center">Cadastrar Produto</h2>
-            <Form>
-                <Form.Group className="mb-3"
-                    id="name">
-                    <Form.Label>Nome</Form.Label>
-                    <Form.Control type="text" placeholder="Digite o nome do produto..."
-                        defaultValue={nome} onChange={e => setNome(e.target.value)} />
-                    <Form.Text className="text-muted"></Form.Text>
-                </Form.Group>
+                <h2 className="font-weight-bold text-center">Cadastrar Produto</h2>
+                <Form>
+                    <Form.Group className="mb-3" id="name">
+                        <Form.Label>Nome</Form.Label>
+                        <Form.Control type="text" placeholder="Digite o nome do produto..."
+                            defaultValue={nome} onChange={e => setNome(e.target.value)} />
+                        <Form.Text className="text-muted"></Form.Text>
+                    </Form.Group>
 
-                <Form.Group className="mb-3" id="price">
-                    <Form.Label>Preço</Form.Label>
-                    <Form.Control type="number" placeholder="Digite o preço..."
-                        defaultValue={preco} onChange={e => setPreco(e.target.value)} />
-                    <Form.Text className="text-muted"></Form.Text>
-                </Form.Group>
+                    <Form.Group className="mb-3" id="price">
+                        <Form.Label>Preço</Form.Label>
+                        <Form.Control type="number" placeholder="Digite o preço..."
+                            defaultValue={preco} onChange={e => setPreco(e.target.value)} />
+                        <Form.Text className="text-muted"></Form.Text>
+                    </Form.Group>
 
-                <Form.Group className="mb-3" id="description">
-                    <Form.Label>Descrição</Form.Label>
-                    <Form.Control type="text" placeholder="Descreva o produto..."
-                        defaultValue={descricao} onChange={e => setDescricao(e.target.value)} />
-                    <Form.Text className="text-muted"></Form.Text>
-                </Form.Group>
+                    <Form.Group className="mb-3" id="description">
+                        <Form.Label>Descrição</Form.Label>
+                        <Form.Control type="text" placeholder="Descreva o produto..."
+                            defaultValue={descricao} onChange={e => setDescricao(e.target.value)} />
+                        <Form.Text className="text-muted"></Form.Text>
+                    </Form.Group>
 
-                <Form.Group className="mb-3" id="category">
-                    <Form.Label>Categoria</Form.Label>
-                    <Form.Control type="text" placeholder="Descreva a categoria do produto..."
-                        defaultValue={categoria} onChange={e => setCategoria(e.target.value)} />
-                    <Form.Text className="text-muted"></Form.Text>
-                </Form.Group>
-                <Link to='/paineldecontrole'>
-                    <Button variant="primary" type="submit" onClick={reqPost}>Cadastrar</Button>
-                </Link>
-            </Form>
+                    <Form.Group className="mb-3" id="category">
+                        <Form.Label>Categoria</Form.Label>
+                        <Form.Control type="text" placeholder="Descreva a categoria do produto..."
+                            defaultValue={categoria} onChange={e => setCategoria(e.target.value)} />
+                        <Form.Text className="text-muted"></Form.Text>
+                    </Form.Group>
+                    <Link to='/paineldecontrole'>
+                        <Button variant="primary" type="submit" onClick={reqPost}>Cadastrar</Button>
+                    </Link>
+                </Form>
         </div>
     )
 }
