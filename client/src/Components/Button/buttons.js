f05c565bfd08bafc766f5629abe75fb40a34a786
woodthestrong <woodthestrong@gmail.com>
2022-09-08 21:46:43 -0300

    projeto-final-grupo-1-3

---
+++ client/src/Components/Button/buttons.js
@@ -0,0 +1,12 @@
+import { Icon } from "@mui/material";
+import React from "react";
+//import { Icon } from '@mui/icons-material/AddShoppingCart';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+export default function ButtonIcon(props) {
+    return(
+        <button onClick={props.onClick} type={props.type} className="buttom" >
+            <AddShoppingCartIcon>{props.icon}</AddShoppingCartIcon>
+        </button>
+    )
+}
