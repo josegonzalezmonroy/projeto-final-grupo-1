a0c8700e01e5e50411b12ba51acab9a3e0f1a825
Gabriela Zapata <gabyzda@gmail.com>
2022-09-13 01:51:13 -0300

    Componente Header, reestruturado Projeto renomeado

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,19 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
-import Header from "./Components/Header/Index";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
 import CadastrarProduto from "./Components/PainelDeControle/CadastrarProduto/CadastrarProduto";
 import ListarProdutosCliente from "./Components/ListarProdutos/ListarProdutosCliente";
 import ListarProdutosPainel from "./Components/PainelDeControle/ListarProdutosPainel/ListarProdutosPainel";
 import EditarProduto from "./Components/PainelDeControle/EditarProduto/EditarProduto";
 //import Cart from "./Components/Context/Cart";
 //import CartProvider from "./Context/Cart";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faShoppingCart,
-  faSun,
-  faHouse,
-} from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <nav className="menu">
-        <div className="button-painel">
-          <Link to="/">
-            <Button variant="light">
-              <FontAwesomeIcon icon={faHouse} />
-            </Button>
-          </Link>
-          <Link to="/listaprodutos">
-            <Button variant="light">Categoria</Button>
-          </Link>
-          <Link to="/paineldecontrole">
-            <Button variant="light">
-              <FontAwesomeIcon icon={faSun} />
-            </Button>
-          </Link>
-
-          <div className="CartShopping" value="produtosCart">
-            <FontAwesomeIcon icon={faShoppingCart} />
-          </div>
-        </div>
-      </nav>
-      <Header title="CGJW Loja de Produtos" />
+      
+      <Header/>
 
       <Routes>
         <Route path="/listaprodutos" element={<ListarProdutosCliente />} />
---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faShoppingCart,
+  faSun,
+  faHouse,
+} from "@fortawesome/free-solid-svg-icons";
+import './Header.css';
+
+ const Header = () => {
+  return (
+    <nav className="menu">
+        <div className="button-painel">
+
+          <div>
+          <Link to="/">
+            <Button variant="dark">
+              <FontAwesomeIcon icon={faHouse} />
+            </Button>
+          </Link>
+          </div>
+
+          <div>
+          <Link to="/listaprodutos">
+            <Button variant="dark">Categoria</Button>
+          </Link>
+          </div>
+
+          <div>
+          <Link to="/paineldecontrole">
+            <Button variant="dark">
+              <FontAwesomeIcon icon={faSun} />
+            </Button>
+          </Link>
+          </div>
+
+          <div id="CartShopping" value="produtosCart">
+            <FontAwesomeIcon icon={faShoppingCart} />
+          </div>
+
+        </div>
+      </nav>
+  )
+ }
+
+export default Header;
