2d3cd94c0231e3b2ed83bdcd91c6f2cae071b561
Gabriela Zapata <gabyzda@gmail.com>
2022-09-15 05:06:42 -0300

    Criados modules Homejs-css e Categoriajs-css

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
 import Footer from "./Components/Footer/Footer";
 import CadastrarProduto from "./Components/PainelDeControle/CadastrarProduto/CadastrarProduto";
 import ListarProdutosCliente from "./Components/ListarProdutos/ListarProdutosCliente";
@@ -8,7 +9,8 @@ import ListarProdutosPainel from "./Components/PainelDeControle/ListarProdutosPa
 import EditarProduto from "./Components/PainelDeControle/EditarProduto/EditarProduto";
 //import Cart from "./Components/Context/Cart";
 //import CartProvider from "./Context/Cart";
-import Pagamento from "./Components/ListarProdutos/Pagamentos/Pagamento"
+import Pagamento from "./Components/ListarProdutos/Pagamentos/Pagamento";
+import Categoria from "./Components/Categoria/Categoria"
 import "./App.css";
 
 export default function App() {
@@ -16,10 +18,9 @@ export default function App() {
     <BrowserRouter>
       
       <Header/>
-
-      {/* <img src="Stock_Camisas.jpg" alt="stock"/> */}
-
+      
       <Routes>
+        <Route path="/" element={<Home/>} />
         <Route path="/listaprodutos" element={<ListarProdutosCliente />} />
         <Route path="/paineldecontrole" element={<ListarProdutosPainel />} />
         <Route
@@ -28,6 +29,7 @@ export default function App() {
         />
         <Route path="/:id" element={<EditarProduto />} />
        <Route path="/Pagamento" element={<Pagamento />} /> 
+       <Route path="/categoria" element={<Categoria/>} />
       </Routes>
 
       <Footer/>
