78f387076a3521c06d343e027760e9ea2488ec1a
Jose <105132443+josegonzalezmonroy@users.noreply.github.com>
2022-09-17 22:33:24 -0300

    Criando a funcionalidade CONFIRM para o DELETE

--- client/src/Components/PainelDeControle/ListarProdutosPainel/ListarProdutosPainel.js
+++ client/src/Components/PainelDeControle/ListarProdutosPainel/ListarProdutosPainel.js
@@ -14,15 +14,21 @@ export default function ListarProdutosPainel() {
             .then(listaProdutos => setProdutos(listaProdutos))
     })
 
-    const apagarProduto = async (urlId) => {
+    const excluirProduto = async (id) => {
 
-        fetch(host + urlId, {
+        fetch(host + id, {
             method: 'DELETE'
-        })
+        }).then(setConfirmar(false))
     }
 
+    const [confirmar, setConfirmar] = useState(false)
+    const [idProduto, setIdProduto] = useState()
+    const [nome, setNome] = useState()
+    const [imagem, setImagem] = useState()
+    const [preco, setPreco] = useState()
+
     return (
-        <div>
+        <div className="painel">
             <Link to="/paineldecontrole/cadastrarproduto">
                 <div className="cadastro">
                     <Button variant="dark">
@@ -30,28 +36,47 @@ export default function ListarProdutosPainel() {
                     </Button>
                 </div>
             </Link>
-
             <div className="cards">
-                {produtos.map((produto) => {
-                    return (
-                        <div className="card-item" key={produto._id}>
-                            <Card className="Card">
-                                <Card.Img variant="top" src={host + produto.patch}
-                                />
-                                <Card.Body>
-                                    <Card.Title>{produto.nome}</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">R$ {produto.preco}</Card.Subtitle>
-                                    <Card.Text>{produto.descricao}</Card.Text>
-                                    <Link to={`/${produto._id}`}>
-                                        <Button variant="dark" size="sm">Editar</Button>
-                                    </Link>
-                                    <Button variant="secondary" size="sm" onClick={() => apagarProduto(produto._id)}>Apagar</Button>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                    )
-                }
-                )
+                {!confirmar ?
+                    (produtos.map((produto) => {
+                        return (
+                            <div className="card-item" key={produto._id}>
+                                <Card className="Card">
+                                    <Card.Img variant="top" src={host + produto.patch}
+                                    />
+                                    <Card.Body>
+                                        <Card.Title>{produto.nome}</Card.Title>
+                                        <Card.Subtitle className="mb-2 text-muted">R$ {produto.preco}</Card.Subtitle>
+                                        <Card.Text>{produto.descricao}</Card.Text>
+                                        <Link to={`/${produto._id}`}>
+                                            <Button variant="dark" size="sm">Editar</Button>
+                                        </Link>
+                                        <Button variant="secondary" size="sm" onClick={() => {
+                                            setConfirmar(true)
+                                            setIdProduto(produto._id)
+                                            setNome(produto.nome)
+                                            setImagem(host + produto.patch)
+                                            setPreco(produto.preco)
+                                        }}>Excluir</Button>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        )
+                    }
+                    )) :
+                    <div className="confirmar">
+                        <h1>Tem certeza que deseja excluir este produto?</h1>
+                        <Card className="Card">
+                            <Card.Img variant="top" src={imagem} />
+                            <Card.Body>
+                                <Card.Title>{nome}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">R$ {preco}</Card.Subtitle>
+
+                                <Button variant="secondary" size="sm" onClick={() => excluirProduto(idProduto)}>Excluir</Button>
+                                <Button variant="secondary" size="sm" onClick={() => setConfirmar(false)}>Cancelar</Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
                 }
             </div>
         </div>
