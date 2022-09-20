7d55478ad59ffdfae75e3d5eb6af48f78b056a16
woodthestrong <woodthestrong@gmail.com>
2022-09-19 23:51:42 -0300

    Ajustando a tela de detalhe de produtos

--- client/src/Components/Produtos/Detalhes/Detalhes.js
+++ client/src/Components/Produtos/Detalhes/Detalhes.js
@@ -27,15 +27,15 @@ export default function Detalhes() {
   const id = urlId.substring(10, urlId.length); //Para pegar um elemento e seus detalhes
 
   return (
-    <div>
-      <div className="row">
+    <div className="vazio">
+      <div className="align-title">
         <h2 className="font-weight-bold text-center">Detalhes do produto</h2>
         <div
           style={{
-            display: "flex",
-            padding: 30,
-            marginLeft: 800,
-            marginTop: -65,
+            //display: "flex",
+            //padding: 30,
+            //marginLeft: 800,
+            marginTop: 12,
           }}
         >
           <Badge color="secondary" badgeContent={itemCount}>
@@ -46,77 +46,105 @@ export default function Detalhes() {
       <div className="cards">
         {produtos.map((produto) => {
           return (
-            <div className="card-item" key={produto._id}>
-              <Card style={{ width: "12.75rem" }}>
-                <Card.Img variant="top" src={host + produto.patch} />
-                <br></br>
-                {/*Estrelas de avaliação */}
-                <div style={{ display: "block", padding: 10 }}>
-                  <Box component="fieldset" borderColor="transparent">
-                    <Rating
-                      name="Rating Label"
-                      value={ratingValue}
-                      onChange={(event, newValue) => {
-                        setRatingValue(newValue);
-                      }}
-                    />
-                  </Box>
+            
+            <div key={produto._id} className="central">
+              <div className="card-item">
+                <div className="img-card">
+                  <Card.Img className="card-image" variant="top" src={host + produto.patch} />
+                </div>
                 </div>
                 {/*Nome, preço e descrição dos produtos */}
-                <Card.Body>
-                  <Card.Title>{produto.nome}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    R$ {produto.preco}
-                  </Card.Subtitle>
-                  <Card.Text>{produto.descricao}</Card.Text>
-                  <p>
-                    <strong>
-                      <i>Frete grátis</i>
-                    </strong>
-                  </p>
+                <div className="card-body">
+                  <Card.Body >
+                    {/*Estrelas de avaliação */}
+                    <div className="estrelas">
+                      <Box component="fieldset" borderColor="transparent">
+                        <Rating
+                          name="Rating Label"
+                          value={ratingValue}
+                          onChange={(event, newValue) => {
+                            setRatingValue(newValue);
+                          }}
+                        />
+                      </Box>
+                    </div>
+                                      
+                    
+                    <div  className="card-nome">
+                      <Card.Title>
+                        {produto.nome}
+                      </Card.Title>
+                    </div>
+                    
+                    <div className="card-preco">
+                      <Card.Subtitle >
+                        R$ {produto.preco}
+                      </Card.Subtitle>
+                    </div>
+                    <div className="card-texto">
+                      <Card.Text>
+                        {produto.descricao}
+                      </Card.Text>
+                    </div>
+                    <div className="card-cor">
+                      {produto.cor}
+                    </div>
+                    <div className="card-tamanho">
+                      {produto.tamanho}
+                    </div>
+                    <p className="frete">
+                      <strong>
+                        <i>Frete grátis</i>
+                      </strong>
+                    </p>
 
-                  {/*Botões de adicionar ou remover Carinho de compras*/}
-                  <div
-                    style={{
-                      display: "block",
-                      padding: 1,
-                      alignItems: "center",
-                      marginLeft: -5,
-                    }}
-                  >
-                    <div className="botoes">
-                      <ButtonGroup>
-                        <Button
-                          variant="secondary"
-                          size="sm"
-                          onClick={() => {
-                            setItemCount(Math.max(itemCount - 1, 0));
-                          }}
-                        >
-                          <RemoveIcon fontSize="xx-small" />
-                        </Button>
-                        <Button
-                          variant="secondary"
-                          size="sm"
-                          onClick={() => {
-                            setItemCount(itemCount + 1);
-                          }}
-                        >
-                          <AddIcon fontSize="xx-small" />
-                        </Button>
-                        <Link to="/Pagamento">
-                          <div className="Pagamento">
-                            <Button size="sm" variant="dark">
-                              Comprar
+                    {/*Botões de adicionar ou remover Carinho de compras*/}
+                    <div
+                      style={{
+                        display: "block",
+                        padding: 1,
+                        alignItems: "center",
+                        marginLeft: -5,
+                      }}
+                    >
+                      <div className="botoes">
+                        <ButtonGroup className="grupo-botoes">
+                          <div className="botao-menos">
+                            <Button
+                              variant="secondary"
+                              size="sm"
+                              onClick={() => {
+                                setItemCount(Math.max(itemCount - 1, 0));
+                              }}
+                            >
+                              <RemoveIcon fontSize="xx-small" />
+                              </Button>
+                          </div>
+                          <div className="botao-mais">
+                            <Button
+                              variant="secondary"
+                              size="sm"
+                              onClick={() => {
+                                setItemCount(itemCount + 1);
+                              }}
+                            >
+                            <AddIcon fontSize="xx-small" />
                             </Button>
                           </div>
-                        </Link>
-                      </ButtonGroup>
+                          <Link to="/Pagamento">
+                            <div className="Pagamento">
+                              <Button size="sm" variant="dark">
+                                Comprar
+                              </Button>
+                            </div>
+                          </Link>
+                        </ButtonGroup>
+                      </div>
                     </div>
-                  </div>
-                </Card.Body>
-              </Card>
-            </div>
+                  </Card.Body>
+                </div>              
+              </div>
+                      
           );
         })}
       </div>
