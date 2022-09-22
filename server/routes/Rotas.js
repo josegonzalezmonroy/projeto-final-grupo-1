c375c73244000ecef73e03dcfc2b653dc291c5a8
Jose <105132443+josegonzalezmonroy@users.noreply.github.com>
2022-09-22 18:53:45 -0300

    update

--- server/routes/Rotas.js
+++ server/routes/Rotas.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const produtos = require('../controllers/ProdutosController')
-const carrinho = require('../controllers/CarrinhoControler')
 
 //........................PRODUCT........................//
 router.get('/', produtos.mostrarProdutos)
